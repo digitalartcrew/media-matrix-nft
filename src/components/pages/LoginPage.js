244054bfac5b99c9d8560f2edebab384fa6a7a88
DC Tech Macbook <dctechmacbook@DCs-MacBook-Pro.local>
2022-01-31 22:24:15 -1000

    add a button

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,12 @@
+import Button from "react-bootstrap/Button";
+
 const LoginPage = () => {
-	return (
-		<div>
-			<h1>LoginPage</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      <Button>submit</Button>
+    </div>
+  );
 };
 
 export default LoginPage;
